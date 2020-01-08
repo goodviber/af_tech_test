56dc8ae27533f09b34915090b05bb79a4b3483d0
maxmills <8balldigitalsolutions@gmail.com>
2020-01-08 13:25:47 +0000

    return newline

--- lib/parser_oo.rb
+++ lib/parser_oo.rb
@@ -10,18 +10,20 @@ class ParserOo
     File.foreach(@log) do |line|
       h.merge(PageLister.new(line).yield_page_for_page_views(h))
     end
-    Hash[h.sort_by{|k,v| v}.reverse]
+    result = Hash[h.sort_by{|k,v| v}.reverse]
+    result.each { |k, v| puts "#{k} #{v} visits" }
   end
 
   def list_pages_by_unique_views(i={})
     File.foreach(@log) do |line|
-      i.merge(PageLister.new(line).yield_page_for_unique_views(i))
+      i.merge(PageLister.new(line.strip).yield_page_for_unique_views(i))
     end
-    Hash[i.sort_by{|k,v| v}.reverse]
+    result = Hash[i.sort_by{|k,v| v}.reverse]
+    result.each { |k, v| puts "#{k} #{v} unique views" }
   end
 
   p = ParserOo.new(ARGV[0])
-  puts p.list_pages_in_order
-  puts p.list_pages_by_unique_views
+  p.list_pages_in_order
+  p.list_pages_by_unique_views
 
 end
