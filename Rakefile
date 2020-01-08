e0619466cb59406a69ef958ca3b36cc583368a92
maxmills <8balldigitalsolutions@gmail.com>
2020-01-08 16:54:23 +0000

    rakefile parser example

--- Rakefile
+++ Rakefile
@@ -8,3 +8,15 @@ end
 
 
 task default: :test
+
+# example of rake task to run parser, would need print_results NOT to be a class method.
+desc 'Run Parser'
+  task :parser, [:datafile] do |t, args|
+
+    project_root = File.dirname(File.absolute_path(__FILE__))
+    Dir.glob(project_root + '/lib/*') { |file| require file }
+
+    p = ParserOo.new(args[:datafile])
+    p.print_results(p.list_pages_in_order, "visits")
+    p.print_results(p.list_pages_by_unique_views, "unique views")
+  end
--- lib/parser_oo.rb
+++ lib/parser_oo.rb
@@ -10,20 +10,23 @@ class ParserOo
     File.foreach(@log) do |line|
       h.merge(PageLister.new(line).yield_page_for_page_views(h))
     end
-    result = Hash[h.sort_by{|k,v| v}.reverse]
-    result.each { |k, v| puts "#{k} #{v} visits" }
+    Hash[h.sort_by{|k,v| v}.reverse]
   end
 
   def list_pages_by_unique_views(i={})
     File.foreach(@log) do |line|
       i.merge(PageLister.new(line.strip).yield_page_for_unique_views(i))
     end
-    result = Hash[i.sort_by{|k,v| v}.reverse]
-    result.each { |k, v| puts "#{k} #{v} unique views" }
+    Hash[i.sort_by{|k,v| v}.reverse]
   end
 
+  def self.print_results(result, text)
+    result.each { |k, v| puts "#{k} #{v} #{text}" }
+  end
+
+ # this might be better in the Rakefile to make test output cleaner
   p = ParserOo.new(ARGV[0])
-  p.list_pages_in_order
-  p.list_pages_by_unique_views
+  print_results(p.list_pages_in_order, "visits")
+  print_results(p.list_pages_by_unique_views, "unique views")
 
 end
