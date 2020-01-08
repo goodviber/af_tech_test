580c0cf96921a86fb6627cdb19a09fb91480720f
maxmills <8balldigitalsolutions@gmail.com>
2020-01-08 14:15:34 +0000

    refactoring tests

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ require 'rake/testtask'
 Rake::TestTask.new(:test) do |t|
   t.libs << 'test'
   t.pattern = 'test/**/test_*.rb'
-  t.verbose = false
+  t.verbose = true
 end
 
 
--- test/test_parser_oo.rb
+++ test/test_parser_oo.rb
@@ -5,8 +5,6 @@ require 'tempfile'
 class TestParserOo < MiniTest::Test
 
     def setup
-      @weblog = "data/webserver.log"
-      @subject = ParserOo.new(@weblog)
       @templog = Tempfile.new('log')
       @templog.write("/home 225.183.113.22
         /contact/ 225.183.113.22
@@ -23,6 +21,7 @@ class TestParserOo < MiniTest::Test
         /products/2 155.231.213.36
         /home 245.141.61.189")
       @templog.rewind
+      @subject = ParserOo.new(@templog)
     end
 
     def test_it_instantiates_a_parser
@@ -39,8 +38,7 @@ class TestParserOo < MiniTest::Test
     end
 
     def test_list_pages_in_order_returns_ordered_results
-      parser = ParserOo.new(@templog)
-      result = parser.list_pages_in_order
+      result = @subject.list_pages_in_order
       assert_equal({"/products/2"=>3, "/about"=>3, "/home"=>3, "/contact/"=>2, "products/1"=>1, "/products/3"=>1, "/index"=>1}, result)
     end
 
@@ -54,8 +52,7 @@ class TestParserOo < MiniTest::Test
     end
 
     def test_list_pages_by_unique_views_returns_ordered_results
-      parser = ParserOo.new(@templog)
-      result = parser.list_pages_by_unique_views
-      assert_equal({"        /about 222.28.220\n"=>2, "        /contact/ 225.183.113.22\n"=>2, "        /home 245.141.61.189"=>1, "        /products/2 155.231.213.36\n"=>1, "        /products/2 83.104.119.23\n"=>1, "        /products/2 225.183.113.22\n"=>1, "        products/1 117.198.72.249\n"=>1, "        /products/3 138.222.28.220\n"=>1, "        /about 138.222.28.220\n"=>1, "        /index 122.255.244.161\n"=>1, "        /home 225.183.113.22\n"=>1, "/home 225.183.113.22\n"=>1}, result)
+      result = @subject.list_pages_by_unique_views
+      assert_equal({"/home 225.183.113.22"=>2, "/contact/ 225.183.113.22"=>2, "/about 222.28.220"=>2, "/products/2 225.183.113.22"=>1, "products/1 117.198.72.249"=>1, "/products/2 83.104.119.23"=>1, "/products/3 138.222.28.220"=>1, "/about 138.222.28.220"=>1, "/index 122.255.244.161"=>1, "/products/2 155.231.213.36"=>1, "/home 245.141.61.189"=>1}, result)
     end
 end
